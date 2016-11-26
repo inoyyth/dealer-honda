dd5aec3202c4313e6fb3a8dfd8bcfc72cfdf18d4
inoy <supri170845@gmail.com>
2016-11-26 21:58:50 +0700

    update terima aksesoris

---
+++ application/modules/aksesoris_terima/models/M_aksesoris_terima.php
@@ -0,0 +1,66 @@
+<?php
+
+Class M_aksesoris_terima extends CI_Model {
+
+    var $table = "penerimaan_aksesoris";
+
+    public function save() {
+        $id = $this->input->post('id');
+        $image_hidden = $this->input->post('image_hidden');
+        $folder = "aksesoris";
+        if (!is_dir('./assets/images/' . $folder)) {
+            mkdir('./assets/images/' . $folder, 0777, TRUE);
+        }
+        $image_config = array('upload_path' => './assets/images/' . $folder,
+            'upload_url' => './assets/images/' . $folder,
+            'encrypt_name' => true,
+            'detect_mime' => true,
+            'allowed_types' => 'gif|jpg|png', 
+            'max_size' => 3000);
+        $this->upload->initialize($image_config);
+        if ($this->upload->do_upload('foto')) {
+            $image = $this->upload->data();
+            $image_name = 'assets/images/' . $folder ."/". $image['file_name'];
+        } else {
+            if (isset($image_hidden) && !empty($image_hidden)) {
+                $image_name = $image_hidden;
+            } else {
+                $error = array('error' => $this->upload->display_errors());
+                if(strpos($error['error'],"You did not select a file to upload.")==true){
+                    $image_name = 'assets/images/' . $folder . '/icon.png';
+                }else{
+                    $this->session->set_flashdata('error', $this->upload->display_errors());
+                    redirect("master-aksesoris-tambah");
+                }
+            }
+        }
+
+        $data = array(
+            'kd_aksesoris' => $this->input->post('kd_aksesoris'),
+            'aksesoris' => $this->input->post('aksesoris'),
+            'kategori' => $this->input->post('kategori'),
+            'url_foto' => $image_name,
+            'm_status' => $this->input->post('m_status')
+        );
+        if (empty($id)) {
+            $this->db->insert($this->table, $this->main_model->create_sys($data));
+            return true;
+        } else {
+            $this->db->update($this->table, $this->main_model->update_sys($data), array('id' => $id));
+            return true;
+        }
+        return false;
+    }
+
+    public function getdata($table, $limit, $pg, $like = array(), $where = array()) {
+        unset($like['page']);
+        $this->db->select($this->table.".*,m_aksesoris.aksesoris,m_gudang.gudang");
+        $this->db->from($table);
+        $this->db->join('m_aksesoris', $this->table.'.aksesoris_id=m_aksesoris.id', 'left');
+        $this->db->join('m_gudang', $this->table.'.gudang_id=m_gudang.id', 'left');
+        $this->db->like($like);
+        $this->db->where($where);
+        $this->db->limit($pg, $limit);
+        return $this->db->get()->result_array();
+    }
+}
--- application/modules/md_jabatan/controllers/Md_jabatan.php
+++ application/modules/md_jabatan/controllers/Md_jabatan.php
@@ -6,6 +6,9 @@ class Md_jabatan extends MX_Controller {
 
     public function __construct() {
         parent::__construct();
+        if(!$this->session->userdata('logged_in_admin')){
+            redirect(base_url());
+        }
         $this->load->model(array('M_md_jabatan' => 'm_jabatan'));
         $this->load->library('Printpdf', 'Auth_log');
         //set breadcrumb
