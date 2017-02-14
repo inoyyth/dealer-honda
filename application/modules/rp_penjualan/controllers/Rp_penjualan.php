07e214bc155f58affcc001cb91bb95a311ef4d96
Supriyadin <supriyadin@iproperty.com>
2017-02-14 23:40:05 +0700

    update report penjualan

--- application/modules/rp_penjualan/controllers/Rp_penjualan.php
+++ application/modules/rp_penjualan/controllers/Rp_penjualan.php
@@ -13,9 +13,37 @@ class Rp_penjualan extends MX_Controller {
     }
 
     public function index() {
-        $data = array();
+        $config['base_url'] = base_url() . 'penjualan-page';
+        $config['total_rows'] = $this->main_model->countdata($this->table, $where = array());
+        $config['per_page'] = (!empty($data_session['page']) ? $data_session['page'] : 10);
+        $config['uri_segment'] = 2;
+        $limit = ($this->uri->segment(2)) ? $this->uri->segment(2) : 0;
+        $this->pagination->initialize($config);
+        $data['paging'] = $this->pagination->create_links();
+        $data['data'] = $this->m_report_penjualan->getdata($this->table, $limit, $config['per_page'], $like = [], $where = array());
+        $data['sr_data'] = $data_session;
         $data['view'] = 'rp_penjualan/main';
         $this->load->view('default', $data);
+    }
+    
+    public function __getSession() {
+        if ($_POST) {
+            return $data = array(
+                'page' => set_session_table_search('page', $this->input->get_post('page', TRUE)),
+                't_penjualan' => set_session_table_search('noso', $this->input->get_post('noso', TRUE)),
+                'nomsn' => set_session_table_search('nomsn', $this->input->get_post('nomsn', TRUE)),
+                'harga_otr' => set_session_table_search('harga_otr', $this->input->get_post('harga_otr', TRUE)),
+                'tipe' => set_session_table_search('tipe', $this->input->get_post('tipe', TRUE))
+            );
+        } else {
+            return $data = array(
+                'page' => $this->session->userdata('page'),
+                'no_so' => $this->session->userdata('noso'),
+                'nomsn' => $this->session->userdata('nomsn'),
+                'harga_otr' => $this->session->userdata('harga_otr'),
+                'tipe' => $this->session->userdata('tipe')
+            );
+        }
     }
 
 }
--- application/modules/rp_penjualan/models/M_rp_penjualan.php
+++ application/modules/rp_penjualan/models/M_rp_penjualan.php
@@ -4,4 +4,14 @@ Class M_rp_penjualan extends CI_Model {
 
     var $table = "t_penjualan";
 
+    public function getdata($table, $limit, $pg, $like = array(), $where = array()) {
+        unset($like['page']);
+        $this->db->select($table.".*,penerimaan_motor.tipe");
+        $this->db->from($table);
+        $this->db->join('penerimaan_motor',$table.".nomsn=penerimaan_motor.nomesin");
+        $this->db->like($like);
+        $this->db->where($where);
+        $this->db->limit($pg, $limit);
+        return $this->db->get()->result_array();
+    }
 }
