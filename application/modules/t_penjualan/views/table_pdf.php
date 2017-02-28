c4d45c41f1c084755f79d70a752bfb47ce309d12
inoy <supri170845@gmail.com>
2017-03-01 00:35:36 +0700

    fix error on penjualan # to be continue

--- application/modules/t_penjualan/views/table_pdf.php
+++ application/modules/t_penjualan/views/table_pdf.php
@@ -1,20 +1,41 @@
 <div style="font-size:  10px;">
     <table border="1" cellpadding="2">
-    <tr>
-        <td align="center">Kode</td>
-        <td align="center">Nama</td>
-        <td align="center">Alamat</td>
-        <td align="center">Telepon</td>
-        <td align="center">Status</td>
-    </tr>
-    <?php foreach($list as $kList=>$vList){ ?>
         <tr>
-            <td><?php echo $vList['kd_gudang'];?></td>
-            <td><?php echo $vList['gudang'];?></td>
-            <td><?php echo $vList['alamat'];?></td>
-            <td><?php echo $vList['telepon'];?></td>
-            <td><?php echo get_status($vList['status_gudang']);?></td>
+            <td align="center">No.SO</td>
+            <td align="center">NO.Mesin</td>
+            <td align="center">Harga</td>
+            <td align="center">Tipe</td>
+            <td align="center">Cara Beli</td>
+            <td align="center">Status</td>
         </tr>
-    <?php } ?>
-</table>
+        <?php
+        foreach ($list as $kList => $vList) {
+            switch ($vList['m_status']) {
+                case 1:
+                    $status = "On Process";
+                    break;
+                case 2:
+                    $status = "Canceled";
+                    break;
+                case 3:
+                    $status = "Deleted";
+                    break;
+                case 4:
+                    $status = "Void";
+                    break;
+                case 5:
+                    $status = "Finish";
+                    break;
+            }
+        ?>
+            <tr>
+                <td><?php echo $vList['noso']; ?></td>
+                <td><?php echo $vList['nomsn']; ?></td>
+                <td><?php echo formatrp($vList['harga_otr']); ?></td>
+                <td><?php echo $vList['tipe']; ?></td>
+                <td><?php echo $vList['cara_pembelian']; ?></td>
+                <td><?php echo $status; ?></td>
+            </tr>
+        <?php } ?>
+    </table>
 </div>
