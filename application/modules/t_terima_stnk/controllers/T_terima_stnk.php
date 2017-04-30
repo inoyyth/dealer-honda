27728adecf3b56d831af337bdd83d0514f5130bd
inoy <supri170845@gmail.com>
2017-04-30 23:26:20 +0700

    fixed print terima stnk bpkb

--- application/modules/t_terima_stnk/controllers/T_terima_stnk.php
+++ application/modules/t_terima_stnk/controllers/T_terima_stnk.php
@@ -123,14 +123,14 @@ class T_terima_stnk extends MX_Controller {
     
     public function print_detail_new($id) {
         $data['detail'] = $this->db->get_where($this->table, array('id' => $id))->row_array();
-        $data['birojasa'] = $this->main_model->getMaster('m_biro_jasa', $like = array(), $where = array('status_birojasa' => '1'));
         $data['stnk'] = $this->main_model->getMaster('t_stnk', $like = array(), $where = array('id' => $data['detail']['t_stnk_id']));
+        $data['birojasa'] = $this->main_model->getMaster('m_biro_jasa', $like = array(), $where = array('id' => $data['detail']['birojasa_id']));
         $data['penjualan'] = $this->main_model->getMaster('t_penjualan', $like = array(), $where = array('noso' => $data['stnk'][0]['no_so']));
         $data['customer'] = $this->main_model->getMaster('m_customer', $like = array(), $where = array('no_ktp' => $data['penjualan'][0]['ktp']));
         $data['terima_motor'] = $this->main_model->getMaster('penerimaan_motor', $like = array(), $where = array('nomesin' => $data['penjualan'][0]['nomsn']));
         $data['master_motor'] = $this->main_model->getMaster('m_motor', $like = array(), $where = array('tipe_motor' => $data['terima_motor'][0]['tipe']));
         $data['master_harga_motor'] = $this->main_model->getMaster('t_harga_motor', $like = array(), $where = array('noso' => $data['penjualan'][0]['noso']));
-        //dump($data['stnk'],true);
+        log_message('debug',print_r($data,TRUE));
         $this->load->view('t_terima_stnk/print_detail',$data);
     }
 }
--- application/modules/t_terima_stnk/views/print_detail.php
+++ application/modules/t_terima_stnk/views/print_detail.php
@@ -18,6 +18,13 @@
             top:0;
         }
     }
+    #footer {
+        clear: both;
+        position: relative;
+        z-index: 10;
+        height: 3em;
+        margin-top: -3em;
+    }
 </style>
 <div id="printThis" style="padding: 10px;text-align: justify;width: 900px;">
     <div style="width:94%;">
@@ -29,37 +36,166 @@
         </div>
         <br>
         <div>
-            <table>
+            <table style="margin-left: 20px;" cellpadding="8" cellspacing="3">
                 <tr>
-                    <td>
+                    <td style="width: 100px;">
                         <b>No.Process</b>
                     </td>
-                    <td>
+                    <td style="width: 200px;">
                         : <?php echo $stnk[0]['no_process'];?>
                     </td>
-                    <td>
-                        <b>No.SO</b>
+                    <td style="width: 100px;">
+                        <b>Biro Jasa</b>
                     </td>
-                    <td>
-                        : <?php echo $penjualan[0]['noso'];?>
+                    <td style="width: 200px;">
+                        : <?php echo $birojasa[0]['nama_birojasa'];?>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <b>No.Process</b>
-                    </td>
-                    <td>
-                        : <?php echo $stnk[0]['no_process'];?>
-                    </td>
-                    <td>
                         <b>No.SO</b>
                     </td>
                     <td>
                         : <?php echo $penjualan[0]['noso'];?>
+                    </td>
+                    <td>
+                        <b>Tgl Jadi STNK</b>
+                    </td>
+                    <td>
+                        : <?php echo tanggalan($detail['t_terima_tgl_stnk']);?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Customer</b>
+                    </td>
+                    <td>
+                        : <?php echo $customer[0]['nama_customer'];?>
+                    </td>
+                    <td>
+                        <b>Tgl. Jadi BPKB</b>
+                    </td>
+                    <td>
+                        : <?php echo tanggalan($detail['t_terima_tgl_bpkb']);?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>No.Mesin</b>
+                    </td>
+                    <td>
+                        : <?php echo $penjualan[0]['nomsn'];?>
+                    </td>
+                    <td>
+                        <b>No.Polisi</b>
+                    </td>
+                    <td>
+                        : <?php echo $detail['t_terima_no_polisi'];?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>No.Rangka</b>
+                    </td>
+                    <td>
+                        : <?php echo $terima_motor[0]['norangka'];?>
+                    </td>
+                    <td>
+                        <b>No.STNK</b>
+                    </td>
+                    <td>
+                        : <?php echo $detail['t_terima_no_stnk'];?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Type</b>
+                    </td>
+                    <td>
+                        : <?php echo $terima_motor[0]['tipe'];?>
+                    </td>
+                    <td>
+                        <b>No.BPKB</b>
+                    </td>
+                    <td>
+                        : <?php echo $detail['t_terima_no_bpkb'];?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Warna</b>
+                    </td>
+                    <td>
+                        : <?php echo $terima_motor[0]['warna'];?>
+                    </td>
+                    <td>
+                        <b>No.Bon BJ 1</b>
+                    </td>
+                    <td>
+                        : <?php echo $detail['t_terima_no_bon_1'];?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Tahun</b>
+                    </td>
+                    <td>
+                        : <?php echo $terima_motor[0]['tahun'];?>
+                    </td>
+                    <td>
+                        <b>No.Bon BJ 2</b>
+                    </td>
+                    <td>
+                        : <?php echo $detail['t_terima_no_bon_2'];?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Harga</b>
+                    </td>
+                    <td>
+                        : <?php echo formatrp($penjualan[0]['harga_otr']);?>
+                    </td>
+                    <td>
+                        <b>Biaya Tambahan 1</b>
+                    </td>
+                    <td>
+                        : <?php echo formatrp($detail['t_terima_add_biaya_1']);?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;
+                    </td>
+                    <td>
+                        &nbsp;
+                    </td>
+                    <td>
+                        <b>Biaya Tambahan 2</b>
+                    </td>
+                    <td>
+                        : <?php echo formatrp($detail['t_terima_add_biaya_2']);?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;
+                    </td>
+                    <td>
+                        &nbsp;
+                    </td>
+                    <td valign="top">
+                        <b>Keterangan</b>
+                    </td>
+                    <td>
+                        : <?php echo $detail['t_terima_add_biaya_1_description'];?>
                     </td>
                 </tr>
             </table>
         </div>
+        <br>
+        <br>
+        <br>
         <div>
             <div class="head-covernote" style="float:left;">
                 <b style="font-size: 22px;">PT.MANDALA KEKAR ABADI</b><br>
