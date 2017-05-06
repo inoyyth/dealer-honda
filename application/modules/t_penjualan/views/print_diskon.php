7504e8eea80b9cff227f620af4d0ea27a96af374
supriyadin inoy <supriyadin inoy>
2017-05-06 13:06:13 +0700

    update print dotmatrix

--- application/modules/t_penjualan/views/print_diskon.php
+++ application/modules/t_penjualan/views/print_diskon.php
@@ -3,16 +3,16 @@ $kwt_explode = explode("/", $detail_harga['noso']);
 ?>
 <style>
     body {
-        font-family: "Times New Roman", Times, serif;
-        letter-spacing: 6px;
+        font-family: "Calibri";
+        letter-spacing: 4px;
         font-size: 12px;
-        font-family: Sans-Serif;
     }
     @media print {
         .no-print, .no-print * { display: none !important; }
-        font-family: Sans-Serif;
-        letter-spacing: 6px;
+        font-family: "Calibri";
+        letter-spacing: 4px;
         font-size: 12px;
+        size: 5.5in 8.5in;
     }
 </style>
 <body>
@@ -62,7 +62,7 @@ $kwt_explode = explode("/", $detail_harga['noso']);
         </tr>
         <tr>
             <td style="font-size:12px;width: 25%;border-left:2px solid;border-right:2px solid;border-bottom:2px solid;">Nomor Mesin : </td>
-            <td style="font-size:12px;width: 75%;border-right:2px solid;border-bottom:2px solid;"> <?php echo $detail_penerimaan_motor['nomesin']; ?></td>
+            <td style="font-size:12px;width: 75%;border-right:2px solid;border-bottom:2px solid;letter-spacing: 6px;"> <?php echo $detail_penerimaan_motor['nomesin']; ?></td>
         </tr>
     </table>
     <div style="text-align: right;padding-right: 30px;">
